d0f5e44b41dffadcbe376cf755a60926ef185f3c
Jason Cheng <jason.z.cheng@uk.tesco.com>
2016-11-06 22:20:07 +0000

    Initiating

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+	entry   : './front-end/index.js',
+	output  : {
+		path     : __dirname + '/public/js',
+		filename : 'bundle.js',
+		publicPath : '/public/js'
+	},
+	module  : {
+		loaders : [
+			{
+				test     : /\.js$/,
+				exclude  : /node_modules/,
+				loaders  : ['babel']
+			}
+		]
+	},
+	devServer : {
+		contentBase : './public',
+		hot : true
+	}
+};
